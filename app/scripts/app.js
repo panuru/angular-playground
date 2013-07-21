0b7199e053181729ad817d977349475946f66a3e
panuru <iseesomuch@gmail.com>
2013-07-21 21:34:08 +0400

    +yeoman scaffolds

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularPlaygroundApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
