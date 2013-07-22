cb648083564a0c98e4c7b451a0ef9d49e8f3b9f9
panuru <iseesomuch@gmail.com>
2013-07-22 21:11:31 +0400

    +stupid cross-domain request

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,91 +1,96 @@
 'use strict';
 
 angular.module('angularPlaygroundApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.menus = [
-        {
-          id: 1,
-          url: "top",
-          title: "Верхнее меню",
-          description: "Верхнее меню",
-          ancestry: null
-        },
-        {
-          id: 2,
-          url: "home",
-          title: "Главная",
-          description: "",
-          ancestry: "1"
-        },
-        {
-          id: 3,
-          url: "about",
-          title: "О компании",
-          description: "",
-          ancestry: "1"
-        },
-        {
-          id: 4,
-          url: "portfolio",
-          title: "Портфолио",
-          description: "",
-          ancestry: "1"
-        },
-        {
-          id: 5,
-          url: "blog",
-          title: "Блог",
-          description: "Список новостей",
-          ancestry: "1"
-        },
-        {
-          id: 6,
-          url: "main",
-          title: "Основное меню",
-          description: "Основное меню",
-          ancestry: null
-        },
-        {
-          id: 7,
-          url: "products",
-          title: "Продукты",
-          description: "",
-          ancestry: "6"
-        },
-        {
-          id: 8,
-          url: "services",
-          title: "Услуги",
-          description: "",
-          ancestry: "6"
-        },
-        {
-          id: 9,
-          url: "techreqs",
-          title: "Тех.требования",
-          description: "",
-          ancestry: "6"
-        },
-        {
-          id: 10,
-          url: "pricelist",
-          title: "Прайс",
-          description: "",
-          ancestry: "6"
-        },
-        {
-          id: 11,
-          url: "contacts",
-          title: "Контакты",
-          description: "",
-          ancestry: "6"
-        },
-        {
-          id: 12,
-          url: "products/shtendery",
-          title: "Штендеры (стритлайны)",
-          description: "",
-          ancestry: "6/7"
-        }
-      ];
+  .controller('MainCtrl', function ($scope, $http) {
+      $http.get('http://freshapp.freshintel.ru/api/menus').success(function(data) {
+        $scope.menus = data;
+      }).error(function(err) {
+        alert(err);
+      });
+    // $scope.menus = [
+    //     {
+    //       id: 1,
+    //       url: "top",
+    //       title: "Верхнее меню",
+    //       description: "Верхнее меню",
+    //       ancestry: null
+    //     },
+    //     {
+    //       id: 2,
+    //       url: "home",
+    //       title: "Главная",
+    //       description: "",
+    //       ancestry: "1"
+    //     },
+    //     {
+    //       id: 3,
+    //       url: "about",
+    //       title: "О компании",
+    //       description: "",
+    //       ancestry: "1"
+    //     },
+    //     {
+    //       id: 4,
+    //       url: "portfolio",
+    //       title: "Портфолио",
+    //       description: "",
+    //       ancestry: "1"
+    //     },
+    //     {
+    //       id: 5,
+    //       url: "blog",
+    //       title: "Блог",
+    //       description: "Список новостей",
+    //       ancestry: "1"
+    //     },
+    //     {
+    //       id: 6,
+    //       url: "main",
+    //       title: "Основное меню",
+    //       description: "Основное меню",
+    //       ancestry: null
+    //     },
+    //     {
+    //       id: 7,
+    //       url: "products",
+    //       title: "Продукты",
+    //       description: "",
+    //       ancestry: "6"
+    //     },
+    //     {
+    //       id: 8,
+    //       url: "services",
+    //       title: "Услуги",
+    //       description: "",
+    //       ancestry: "6"
+    //     },
+    //     {
+    //       id: 9,
+    //       url: "techreqs",
+    //       title: "Тех.требования",
+    //       description: "",
+    //       ancestry: "6"
+    //     },
+    //     {
+    //       id: 10,
+    //       url: "pricelist",
+    //       title: "Прайс",
+    //       description: "",
+    //       ancestry: "6"
+    //     },
+    //     {
+    //       id: 11,
+    //       url: "contacts",
+    //       title: "Контакты",
+    //       description: "",
+    //       ancestry: "6"
+    //     },
+    //     {
+    //       id: 12,
+    //       url: "products/shtendery",
+    //       title: "Штендеры (стритлайны)",
+    //       description: "",
+    //       ancestry: "6/7"
+    //     }
+    //   ];
   });
